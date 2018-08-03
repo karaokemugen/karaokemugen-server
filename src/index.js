45551646a8a2b5387af4e3ce8b7ab775ded46e66
Guillaume Lebigot <axel@teri-chan.net>
2018-08-03 10:36:01 +0200

    Fixed CLI options parsing

--- src/index.js
+++ src/index.js
@@ -2,20 +2,17 @@ import {initConfig} from './_utils/config';
 import logger from 'winston';
 import {join} from 'path';
 import {initFrontend} from './frontend';
-import yargs from 'yargs';
+import cli from 'commander';
 import detect from 'detect-port';
 import {initDB, closeDB} from './_dao/database';
 import {run} from './_dao/generation';
+import sudoBlock from 'sudo-block';
 
 const pjson = require('../package.json');
 const appPath = join(__dirname,'../');
 
 process.on('uncaughtException', (exception) => {
 	console.log(exception);
-});
-
-process.on('unhandledRejection', (reason, p) => {
-	console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
 });
 
 main().catch(err => {
@@ -25,15 +22,16 @@ main().catch(err => {
 });
 
 async function main() {
-	const argv = yargs.argv;
-	await initConfig(appPath, argv);
+	sudoBlock('You should not run Karaoke Mugen Server with root permissions, it\'s dangerous.');
+	const argv = parseArgs();
+	await initConfig(appPath, cli);
 	console.log('--------------------------------------------------------------------');
 	console.log(`Karaoke Mugen Server ${pjson.version}`);
 	console.log('--------------------------------------------------------------------');
 	console.log('\n');
 
 	const opts = {
-		port: argv.port || 1350,
+		port: +argv.port || 1350,
 	};
 	await initDB();
 	if (argv.generate) {
@@ -52,4 +50,22 @@ function exit(rc) {
 	}).catch(() => {
 		process.exit(1);
 	});
+}
+
+function parseArgs() {
+	/*
+	if (process.argv.indexOf('--') >= 0) {
+		return process.argv.slice(3);
+	} else {
+		return process.argv.slice(2);
+	}
+	*/
+	const argv = process.argv.filter(e => e !== '--');
+	return cli
+		.command('kmserver.sh')
+		.description('Starts Karaoke Mugen Server')
+		.version(pjson.version)
+		.option('-p, --port [port]', 'specify which port to listen to', 'port')
+		.option('-g, --generate', 'generate karaoke database')
+		.parse(argv);
 }
