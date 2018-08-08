d10f8e689c7aec590050060d0d2fcbf11a212c01
Guillaume Lebigot <axel@teri-chan.net>
2018-08-08 14:32:00 +0200

    Shortener: removed check and added debug info

--- src/_services/shortener.js
+++ src/_services/shortener.js
@@ -1,23 +1,20 @@
 import {insertInstance, updateInstance, selectInstance} from '../_dao/shortener';
+import logger from 'winston';
 
 export async function publishInstance(ip, data) {
 	const currentDate = new Date();
+	logger.debug(`[Shortener] Received publish request from ${ip} with ${JSON.stringify(data)}`);
 	const instance = await selectInstance(ip);
+	logger.debug(`[Shortener] Instance found : ${JSON.stringify(instance)}`);
 	if (instance.length > 0) {
-		if (instance[0].instance_id === data.IID) {
-			// Update
-			await updateInstance({
-				instance_id: data.IID,
-				remote_ip: ip,
-				date: currentDate,
-				local_ip: data.localIP,
-				local_port: data.localPort
-			});
-			return true;
-		} else {
-			// Error
-			throw 'Instance not recognized';
-		}
+		await updateInstance({
+			instance_id: data.IID,
+			remote_ip: ip,
+			date: currentDate,
+			local_ip: data.localIP,
+			local_port: data.localPort
+		});
+		return true;
 	} else {
 		await insertInstance({
 			instance_id: data.IID,
@@ -31,7 +28,9 @@ export async function publishInstance(ip, data) {
 }
 
 export async function getInstance(ip) {
+	logger.debug(`[Shortener] Received get request from ${ip}`);
 	const instance = await selectInstance(ip);
+	logger.debug(`[Shortener] Found instance data ${JSON.stringify(instance)}`);
 	if (instance.length > 0) return instance[0];
 	return false;
 }
