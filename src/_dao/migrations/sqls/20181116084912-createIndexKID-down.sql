e5532452df54a4eb5b6488574e1f87c8fad06558
Guillaume Lebigot <axel@teri-chan.net>
2018-11-16 09:54:22 +0100

    Create indexes migration on KID and SID

---
+++ src/_dao/migrations/sqls/20181116084912-createIndexKID-down.sql
@@ -0,0 +1,4 @@
+/* Replace with your SQL commands */
+
+DROP INDEX idx_serie_sid;
+DROP INDEX idx_kara_kid;
---
+++ src/_dao/migrations/sqls/20181116084912-createIndexKID-up.sql
@@ -0,0 +1,4 @@
+/* Replace with your SQL commands */
+
+CREATE UNIQUE INDEX idx_kara_kid ON kara (kid);
+CREATE UNIQUE INDEX idx_serie_sid on serie (sid);
