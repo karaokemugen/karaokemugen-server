cb1dcab446744a0db66be5f0e747f240e2f50657
Axel Terizaki <axel@teri-chan.net>
2018-12-01 16:14:44 +0100

    Add unique constraint for option

---
+++ src/_dao/migrations/sqls/20181201151214-addUniqueConstraintSettings-down.sql
@@ -0,0 +1,3 @@
+/* Replace with your SQL commands */
+
+ALTER TABLE settings DROP CONSTRAINT unique_option;
---
+++ src/_dao/migrations/sqls/20181201151214-addUniqueConstraintSettings-up.sql
@@ -0,0 +1,3 @@
+/* Replace with your SQL commands */
+
+ALTER TABLE settings ADD CONSTRAINT unique_option UNIQUE (option);
