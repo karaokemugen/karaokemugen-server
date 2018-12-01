8d337f8fc845540f0d8123765c10199a4f948543
Axel Terizaki <axel@teri-chan.net>
2018-12-01 01:28:33 +0100

    Trying new validation fixes for stats

--- src/_utils/validators.js
+++ src/_utils/validators.js
@@ -64,7 +64,8 @@ function songItemValidator(value) {
 	const uuid = new RegExp(uuidRegexp);
 	for (const item of value) {
 		if (!uuid.test(item.kid)) return ` '${value} is invalid (not a valid KID)`;
-		if (isNaN(+item.requested_at)) return ` '${value} is invalid (not a valid requested_at time)`;
+		if (item.requested_at && isNaN(+item.requested_at)) return ` '${value} is invalid (not a valid requested_at time)`;
+		if (item.modified_at && isNaN(item.modified_at)) return ` '${value} is invalid (not a valid modified_at time)`;
 		if (isNaN(item.session_started_at)) return ` '${value} is invalid (not a valid session_started_at time)`;
 	}
 	return null;
