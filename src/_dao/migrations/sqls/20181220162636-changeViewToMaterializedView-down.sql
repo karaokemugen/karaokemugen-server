f7e2aef0004636f20f622a1320649a22f686527d
Axel Terizaki <axel@teri-chan.net>
2018-12-26 11:34:00 +0100

    Revert "Revert "Reworked all_karas materialized view to include more tag info (#11)""
    This reverts commit 839a4027e6daea50dc57e120f7be218afbe69d36.

--- src/_dao/migrations/sqls/20181220162636-changeViewToMaterializedView-down.sql
+++ src/_dao/migrations/sqls/20181220162636-changeViewToMaterializedView-down.sql
@@ -13,6 +13,38 @@ WITH series_i18n(serie_id, serie_langs) AS (
      SELECT sl.fk_id_serie, array_to_json(array_agg(json_build_object('lang', sl.lang, 'name', sl.name)))
      FROM serie_lang sl
      GROUP BY sl.fk_id_serie
+), t_singer(singers) AS (
+     SELECT array_to_json(array_agg(json_build_object('id', t.tag_id, 'name', t.name, 'slug', t.slug, 'karacount', t.karacount )))
+     FROM all_tags t
+     GROUP BY t.tag_id
+), t_songtype(songtypes) AS (
+     SELECT array_to_json(array_agg(json_build_object('id', t.tag_id, 'name', t.name, 'slug', t.slug, 'karacount', t.karacount )))
+     FROM all_tags t
+     GROUP BY t.tag_id
+), t_creator(creators) AS (
+     SELECT array_to_json(array_agg(json_build_object('id', t.tag_id, 'name', t.name, 'slug', t.slug, 'karacount', t.karacount )))
+     FROM all_tags t
+     GROUP BY t.tag_id
+), t_language(languages) AS (
+     SELECT array_to_json(array_agg(json_build_object('id', t.tag_id, 'name', t.name, 'slug', t.slug, 'karacount', t.karacount )))
+     FROM all_tags t
+     GROUP BY t.tag_id
+), t_misc(misc) AS (
+     SELECT array_to_json(array_agg(json_build_object('id', t.tag_id, 'name', t.name, 'slug', t.slug )))
+     FROM all_tags t
+     GROUP BY t.tag_id
+), t_author(authors) AS (
+     SELECT array_to_json(array_agg(json_build_object('id', t.tag_id, 'name', t.name, 'slug', t.slug, 'karacount', t.karacount )))
+     FROM all_tags t
+     GROUP BY t.tag_id
+), t_group(groups) AS (
+     SELECT array_to_json(array_agg(json_build_object('id', t.tag_id, 'name', t.name, 'slug', t.slug, 'karacount', t.karacount )))
+     FROM all_tags t
+     GROUP BY t.tag_id
+), t_songwriter(songwriters) AS (
+     SELECT array_to_json(array_agg(json_build_object('id', t.tag_id, 'name', t.name, 'slug', t.slug, 'karacount', t.karacount )))
+     FROM all_tags t
+     GROUP BY t.tag_id
 )
 
 SELECT
@@ -35,27 +67,26 @@ SELECT
   string_agg(DISTINCT(s.name),',') AS serie,
   jsonb_agg(DISTINCT(s.aliases)) AS serie_altname,
   array_agg(DISTINCT(s.pk_id_serie)) AS serie_id,
-  string_agg(DISTINCT(t_singer.name),',') AS singer,
-  string_agg(DISTINCT(t_songtype.name),',') AS songtype,
-  string_agg(DISTINCT(t_creator.name),',') AS creator,
-  string_agg(DISTINCT(t_language.name),',') AS language,
-  string_agg(DISTINCT(t_author.name),',') AS author,
-  string_agg(DISTINCT(t_misc.name),',') AS misc,
-  string_agg(DISTINCT(t_songwriter.name),',') AS songwriter,
-  string_agg(DISTINCT(t_group.name),',') AS group,
+  jsonb_agg(DISTINCT(t_singer.singers)::jsonb) AS singer,
+  jsonb_agg(DISTINCT(t_songtype.songtypes)::jsonb) AS songtype,
+  jsonb_agg(DISTINCT(t_singer.singers)::jsonb) AS singer,jsonb_agg(DISTINCT(t_language.languages)::jsonb) AS language,
+  jsonb_agg(DISTINCT(t_author.authors)::jsonb) AS author,
+  jsonb_agg(DISTINCT(t_misc.misc)::jsonb) AS misc,
+  jsonb_agg(DISTINCT(t_songwriter.songwriters)::jsonb) AS songwriter,
+  jsonb_agg(DISTINCT(t_group.groups)::jsonb) AS group,
   array_agg(DISTINCT(kt.fk_id_tag)) AS all_tags_id
 FROM kara k
 LEFT JOIN kara_serie ks ON k.pk_id_kara = ks.fk_id_kara
 LEFT JOIN serie s ON ks.fk_id_serie = s.pk_id_serie
 LEFT JOIN series_i18n s18 ON s18.serie_id = ks.fk_id_serie
 LEFT JOIN kara_tag kt ON k.pk_id_kara = kt.fk_id_kara
-LEFT JOIN tag t_singer ON kt.fk_id_tag = t_singer.pk_id_tag AND t_singer.tagtype = 2
-LEFT JOIN tag t_songtype ON kt.fk_id_tag = t_songtype.pk_id_tag AND t_songtype.tagtype = 3
-LEFT JOIN tag t_creator ON kt.fk_id_tag = t_creator.pk_id_tag AND t_creator.tagtype = 4
-LEFT JOIN tag t_language ON kt.fk_id_tag = t_language.pk_id_tag AND t_language.tagtype = 5
-LEFT JOIN tag t_author ON kt.fk_id_tag = t_author.pk_id_tag AND t_author.tagtype = 6
-LEFT JOIN tag t_misc ON kt.fk_id_tag = t_misc.pk_id_tag AND t_misc.tagtype = 7
-LEFT JOIN tag t_group ON kt.fk_id_tag = t_group.pk_id_tag AND t_group.tagtype = 9
-LEFT JOIN tag t_songwriter ON kt.fk_id_tag = t_songwriter.pk_id_tag AND t_songwriter.tagtype = 8
+LEFT JOIN t_singer ON kt.fk_id_tag = t_singer.pk_id_tag AND t_singer.tagtype = 2
+LEFT JOIN t_songtype ON kt.fk_id_tag = t_songtype.pk_id_tag AND t_songtype.tagtype = 3
+LEFT JOIN t_creator ON kt.fk_id_tag = t_creator.pk_id_tag AND t_creator.tagtype = 4
+LEFT JOIN t_language ON kt.fk_id_tag = t_language.pk_id_tag AND t_language.tagtype = 5
+LEFT JOIN t_author ON kt.fk_id_tag = t_author.pk_id_tag AND t_author.tagtype = 6
+LEFT JOIN t_misc ON kt.fk_id_tag = t_misc.pk_id_tag AND t_misc.tagtype = 7
+LEFT JOIN t_group ON kt.fk_id_tag = t_group.pk_id_tag AND t_group.tagtype = 9
+LEFT JOIN t_songwriter ON kt.fk_id_tag = t_songwriter.pk_id_tag AND t_songwriter.tagtype = 8
 GROUP BY k.pk_id_kara
 ORDER BY language, serie, singer, songtype DESC, songorder;
