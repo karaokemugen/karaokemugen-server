978c0f0c43b013b43e3fca7c10105f97b0d04d80
Mathieu Delestre <ldbglobe@gmail.com>
2019-02-06 17:55:23 +0100

    dao serie filter explicist cast as UUID

--- src/_dao/kara.js
+++ src/_dao/kara.js
@@ -45,7 +45,7 @@ export function buildTypeClauses(mode, value) {
 			// Splitting only after the first ":"
 			const type = c.split(/:(.+)/)[0];
 			const values = c.split(/:(.+)/)[1];
-			if (type === 's') search = `${search} AND serie_id @> ARRAY[${values}]`;
+			if (type === 's') search = `${search} AND serie_id @> ARRAY['${values}'::uuid]`;
 			if (type === 'y') search = `${search} AND year IN (${values})`;
 			if (type === 't') search = `${search} AND all_tags_id @> ARRAY[${values}]`;
 		}
