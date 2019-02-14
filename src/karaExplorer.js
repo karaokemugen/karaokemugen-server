3ebd412c2bf395423867662da5a25afa7ef62789
Axel Terizaki <axel@teri-chan.net>
2019-02-14 19:12:58 +0100

    Added logging and resolved startup path for KMExplorer (#11)

--- src/karaExplorer.js
+++ src/karaExplorer.js
@@ -1,14 +1,7 @@
 import pm2 from 'pm2';
 import { getConfig } from './_utils/config';
 import {resolve} from 'path';
-import {asyncWriteFile, asyncMove, asyncExists, asyncUnlink} from './_utils/files';
-import {stringify} from 'ini';
-import {generate} from 'randomstring';
-import fp from 'find-free-port';
-import {connect} from 'socket.io-client';
-import {createServer} from 'net';
 import logger from 'winston';
-import { basename } from 'path';
 
 var uniqid = function() {
 	return (new Date().getTime() + Math.floor((Math.random()*10000)+1)).toString(16);
@@ -18,20 +11,20 @@ export default class KaraExplorer {
 
 	constructor (opts) {
 		this.conf = getConfig();
-		this.appPath = resolve(this.conf.appPath, this.conf.Path.KaraokeMugenApp);
+		this.appPath = resolve(this.conf.appPath);
 		this.app = null;
 
-		this.id = 'KMX_'+uniqid();
+		this.id = uniqid();
 		this.api = opts.api ? opts.api : 'http://localhost:1350';
 		this.port = opts.port ? opts.port : 3000;
 		this.path = opts.path ? opts.path : '/';
 	}
 
 	start = async() => {
-		return new Promise((resolve, reject) => {
+		return new Promise((OK, NOK) => {
 			pm2.connect(true, (err) => {
 				if (err) {
-					reject(err);
+					NOK(err);
 				} else {
 					logger.info(`[Spawn] [${this.id}] connected to PM2`);
 					pm2.start({
@@ -44,14 +37,17 @@ export default class KaraExplorer {
 							'--path='+this.path,
 						],
 						pid: resolve(this.appPath, `${this.id}.pid`),
-						killTimeout: 10000
+						killTimeout: 10000,
+						output: resolve(this.appPath, 'logs/kmexplorer.log'),
+						error: resolve(this.appPath, 'logs/kmexplorer-err.log')
 					}, (err, apps) => {
 						if (err) {
-							reject(err);
+							NOK(err);
 						} else {
 							this.app = apps;
 							logger.info(`[Spawn] Started [${this.id}]`);
 							pm2.disconnect();
+							OK();
 						}
 					});
 				}
