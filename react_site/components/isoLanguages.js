743a93a7b0e9f125da15f76d9936129a9880b73c
Mathieu Delestre <ldbglobe@gmail.com>
2019-02-26 14:34:48 +0100

    Handle "und" and "zxx" language tag

--- react_site/components/isoLanguages.js
+++ react_site/components/isoLanguages.js
@@ -1,3 +1,4 @@
+import { i18n } from '../i18n'
 import isoLanguages from "@cospired/i18n-iso-languages";
 isoLanguages.registerLocale(require("@cospired/i18n-iso-languages/langs/en.json"));
 isoLanguages.registerLocale(require("@cospired/i18n-iso-languages/langs/fr.json"));
@@ -13,6 +14,11 @@ export default function(code,current_language){
 	}
 	else
 	{
+		if(code==='und')
+			return i18n.t("tag:language.und");
+		else if(code==='zxx')
+			return i18n.t("tag:language.zxx");
+
 		r = isoLanguages.getName(code, current_language)
 		if(r && r.length)
 			return r;
