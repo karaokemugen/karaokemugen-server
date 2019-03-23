b267deed1022d719e22a630352f57d045241fe07
Aeden <florentius.b@gmail.com>
2019-03-24 00:21:48 +0100

    Delete menu & useless dependencies

--- kmimport/src/App.js
+++ kmimport/src/App.js
@@ -1,13 +1,4 @@
 import React, {Component} from 'react';
-
-import {Provider} from 'react-redux';
-import {ConnectedRouter} from 'react-router-redux';
-import {Redirect, Route, Switch} from 'react-router-dom';
-import {Layout} from 'antd';
-import {history, store} from './store';
-
-import KMHeader from './layout/KMHeader';
-
 import KaraForm from './pages/KaraForm';
 
 import './App.css';
@@ -16,17 +7,7 @@ class App extends Component {
 
 	render() {
 		return (
-			<Provider store={store}>
-				<ConnectedRouter history={history}>
-					<Layout className="layout">
-						<KMHeader/>
-						<Switch>
-							<Redirect exact from='/' to='/karaimport'/>
-							<Route path='/karaimport' component={KaraForm}/>
-						</Switch>
-					</Layout>
-				</ConnectedRouter>
-			</Provider>
+			<KaraForm />
 		);
 	}
 }
