b541b7b4a1d23198a0f0523f1646f9ebc3341c64
Aeden <florentius.b@gmail.com>
2019-03-24 15:42:04 +0100

    Ajout du module i18n

--- kmimport/src/App.js
+++ kmimport/src/App.js
@@ -1,17 +1,38 @@
-import React, {Component} from 'react';
+import React, { Suspense } from 'react';
 import KaraForm from './pages/KaraForm';
+import {Layout, Menu} from 'antd';
+import { useTranslation } from 'react-i18next';
+import Spinner from 'react-spinner';
 
 import './App.css';
 
-class App extends Component {
+function MyApp() {
+	const { t, i18n } = useTranslation();
 
-	render() {
-		return (
-			<div className="import-kara">
-				<KaraForm />
-			</div>
-		);
-	}
+	return (
+		<div className="import-kara">
+			<Layout.Header style={{height: '56px'}}>
+				<Menu
+					theme="dark"
+					mode="horizontal"
+				>
+					<Menu.Item href="/base">{t('HOME')}</Menu.Item>
+					<label className='label-lang'>Languages :</label>
+					<Menu.Item key="lng-en" onClick={() => i18n.changeLanguage('en')}>EN</Menu.Item>
+					<Menu.Item key="lng-fr" onClick={() => i18n.changeLanguage('fr')}>FR</Menu.Item>
+				</Menu>
+			</Layout.Header>
+			<KaraForm />
+		</div>
+	);
 }
 
 export default App;
+
+function App() {
+	return (
+	  <Suspense fallback={<Spinner />}>
+		<MyApp />
+	  </Suspense>
+	);
+  }
--- kmimport/src/index.js
+++ kmimport/src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import './i18n';
+
 ReactDOM.render(<App />, document.getElementById('root'));
