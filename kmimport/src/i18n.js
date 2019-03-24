723325860ea2bc8f2c5dff8f26ae817f8368bed2
Aeden <florentius.b@gmail.com>
2019-03-24 21:04:59 +0100

    remove debug

--- kmimport/src/i18n.js
+++ kmimport/src/i18n.js
@@ -17,7 +17,6 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: 'en',
-    debug: true,
   
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
