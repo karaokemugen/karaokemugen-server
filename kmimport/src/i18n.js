df93b11c355f7d4e21fda8627d27b2b0782e8a0e
Aeden <florentius.b@gmail.com>
2019-03-24 21:04:29 +0100

    fix path locales

--- kmimport/src/i18n.js
+++ kmimport/src/i18n.js
@@ -21,6 +21,9 @@ i18n
   
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
+    },
+    backend: {
+      loadPath: 'locales/{{lng}}/{{ns}}.json'
     }
   });
 
