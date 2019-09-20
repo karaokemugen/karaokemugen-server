c6d53980504636fd20ab230e45ac551c0015d98f
Guillaume Lebigot <axel@teri-chan.net>
2019-09-20 10:53:06 +0200

    Readding AllKaras indexes

---
+++ migrations/sqls/20190920084822-ReaddAllKarasIndexes-down.sql
@@ -0,0 +1,10 @@
+DROP INDEX idx_ak_created;
+DROP INDEX idx_ak_serie;
+DROP INDEX idx_ak_serie_singer;
+DROP INDEX idx_ak_songtypes;
+DROP INDEX idx_ak_songorder;
+DROP INDEX idx_ak_title;
+DROP INDEX idx_ak_singer;
+DROP INDEX idx_ak_language;
+DROP INDEX idx_ak_year;
+DROP INDEX idx_ak_kid;
