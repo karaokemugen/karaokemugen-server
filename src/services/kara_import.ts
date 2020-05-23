fe0215ec77c786fb1be660d9bb21599a34a35cae
Guillaume Lebigot <axel@teri-chan.net>
2020-05-23 15:25:29 +0200

    Fix object object in series when posting issues

--- src/services/kara_import.ts
+++ src/services/kara_import.ts
@@ -45,7 +45,7 @@ export async function editKara(kara: Kara): Promise<string> {
 		}
 
 		// Post issue to gitlab
-		const karaName = `${newKara.data.langs[0].name.toUpperCase()} - ${newKara.data.series[0]?.name || newKara.data.singers[0].name} - ${newKara.data.songtypes[0].name}${newKara.data.order || ''} - ${newKara.data.title}`;
+		const karaName = `${newKara.data.langs[0].name.toUpperCase()} - ${newKara.data.series[0]?.name || newKara.data.singers[0]?.name} - ${newKara.data.songtypes[0].name}${newKara.data.order || ''} - ${newKara.data.title}`;
 		const conf = getConfig();
 		let title = conf.Gitlab.IssueTemplate.Edit.Title || 'Edited kara: $kara';
 		logger.debug('[GitLab] Kara: '+JSON.stringify(newKara.data, null, 2));
@@ -57,7 +57,7 @@ export async function editKara(kara: Kara): Promise<string> {
 			.replace('$newVideo', `${!newKara.data.noNewVideo}`)
 			.replace('$author', newKara.data.authors.map(t => t.name).join(', '))
 			.replace('$title', newKara.data.title)
-			.replace('$series', newKara.data.series.join(', '))
+			.replace('$series', newKara.data.series.map(t => t.name).join(', '))
 			.replace('$type', newKara.data.songtypes.map(t => t.name).join(', '))
 			.replace('$order', newKara.data.order || '')
 			.replace('$lang', newKara.data.langs.map(t => t.name).join(', '))
@@ -100,7 +100,7 @@ export async function createKara(kara: Kara) {
 		logger.error(`[KaraImport] Error importing kara : ${err}. Kara Data ${JSON.stringify(kara)}`);
 		throw err;
 	}
-	const karaName = `${newKara.data.langs[0].name.toUpperCase()} - ${newKara.data.series[0] || newKara.data.singers[0].name} - ${newKara.data.songtypes[0].name}${newKara.data.order || ''} - ${newKara.data.title}`;
+	const karaName = `${newKara.data.langs[0].name.toUpperCase()} - ${newKara.data.series[0]?.name || newKara.data.singers[0]?.name} - ${newKara.data.songtypes[0].name}${newKara.data.order || ''} - ${newKara.data.title}`;
 	let title = conf.Gitlab.IssueTemplate.Import.Title || 'New kara: $kara';
 	logger.debug('[GitLab] Kara: '+JSON.stringify(newKara.data, null, 2));
 	title = title.replace('$kara', karaName);
@@ -109,7 +109,7 @@ export async function createKara(kara: Kara) {
 		.replace('$comment', kara.comment || '')
 		.replace('$author', newKara.data.authors.map(t => t.name).join(', '))
 		.replace('$title', newKara.data.title)
-		.replace('$series', newKara.data.series.join(', '))
+		.replace('$series', newKara.data.series.map(t => t.name).join(', '))
 		.replace('$type', newKara.data.songtypes.map(t => t.name).join(', '))
 		.replace('$order', newKara.data.order || '')
 		.replace('$lang', newKara.data.langs.map(t => t.name).join(', '))
