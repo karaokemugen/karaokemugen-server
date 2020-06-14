3ce66ac9ea4888d2f171d997a4e387c74a49c99e
Aeden <florentius.b@gmail.com>
2020-06-14 22:06:28 +0200

    Import dans la library de icons.ts d'uniquement des icones utilisées

--- kmexplorer/plugins/icons.js
+++ kmexplorer/plugins/icons.js
@@ -1,12 +1,28 @@
 import Vue from 'vue';
-import {config, library} from '@fortawesome/fontawesome-svg-core';
-import {FontAwesomeIcon, FontAwesomeLayers} from '@fortawesome/vue-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+import { config, library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon, FontAwesomeLayers } from '@fortawesome/vue-fontawesome';
+import { faLaptop, faChalkboardTeacher, faUserSecret, faQuestionCircle, faTag, faBox, faUpload, faCircle, faPlay, faTasks, faTv, faGlobe, faMicrophoneAlt, faSignature, faPhotoVideo, faProjectDiagram, faChess } from '@fortawesome/free-solid-svg-icons';
 
 // This is important, we are going to let Nuxt.js worry about the CSS
 config.autoAddCss = false;
 
-library.add(fas);
+library.add(faQuestionCircle);
+library.add(faUpload);
+library.add(faCircle);
+library.add(faPlay);
+library.add(faTasks);
+library.add(faTv);
+library.add(faGlobe);
+library.add(faMicrophoneAlt);
+library.add(faSignature);
+library.add(faPhotoVideo);
+library.add(faProjectDiagram);
+library.add(faChess);
+library.add(faLaptop);
+library.add(faChalkboardTeacher);
+library.add(faUserSecret);
+library.add(faBox);
+library.add(faTag);
 
 // Register the component globally
 Vue.component('font-awesome-icon', FontAwesomeIcon);
