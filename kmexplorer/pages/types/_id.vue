25e4d7f1e242cca53a4bcc98f30b813d003d11d0
Aeden <florentius.b@gmail.com>
2020-06-17 19:32:02 +0200

    Add infinite loading for tags: pagesize : 400

--- kmexplorer/pages/types/_id.vue
+++ kmexplorer/pages/types/_id.vue
@@ -1,6 +1,9 @@
 <template>
-	<div class="tags" v-if="type">
-		<tag v-for="tag in tags.content" :key="tag.tid" :type="type" :tag="tag" :i18n="tag.i18n" />
+	<div class="is-ancestor">
+		<div class="tags" v-if="type">
+			<tag v-for="tag in tags.content" :key="tag.tid" :type="type" :tag="tag" :i18n="tag.i18n" />
+		</div>
+		<loading-nanami class="tile is-parent is-12" v-if="loading"></loading-nanami>
 	</div>
 </template>
 
@@ -8,10 +11,12 @@
 import Vue from "vue";
 import Tag from "~/components/Tag.vue";
 import { tagTypes } from "../../assets/constants";
+import LoadingNanami from "../../components/LoadingNanami";
 
 export default Vue.extend({
 	name: "ListTag",
 	components: {
+		LoadingNanami,
 		Tag
 	},
 
@@ -21,11 +26,40 @@ export default Vue.extend({
 				infos: { count: 0, from: 0, to: 0 },
 				content: []
 			},
+			from: 0,
 			loading: false
 		};
 	},
 
-	methods: {},
+	methods: {
+		async loadNextPage() {
+			console.log("load")
+			if (this.tags.infos.to === this.tags.infos.count || this.loading)
+				return ;
+			this.from++;
+			this.loading = true;
+			const { data } = await this.$axios.get(`/api/karas/tags/${tagTypes[this.type].type}`, {
+				params: {
+					from: this.from * 400,
+					size: (this.from + 1) * 400
+				}
+			});
+			this.tags.content.push(...data.content);
+			this.tags.infos.to = data.infos.to;
+			this.loading = false;
+		},
+		scrollEvent() {
+			window.addEventListener("scroll", () => {
+				let bottomOfWindow =
+					document.documentElement.scrollTop + window.innerHeight >
+					document.documentElement.offsetHeight - 400;
+
+				if (bottomOfWindow) {
+					this.loadNextPage();
+				}
+			});
+		}
+	},
 
 	validate({ params }) {
 		return params.id && tagTypes[params.id];
@@ -33,15 +67,21 @@ export default Vue.extend({
 
 	async asyncData({ params, $axios, error, app }) {
 		const { data } = await $axios
-			.get(`/api/karas/tags/${tagTypes[params.id].type}`)
+			.get(`/api/karas/tags/${tagTypes[params.id].type}`, {
+                params: {
+                    from: 0,
+                    size: 400
+                }
+            })
 			.catch(_err =>
 				error({ statusCode: 404, message: app.i18n.t("tag.notfound") })
 			);
-		console.log(params.id)
 		return { tags: data, type: params.id };
 	},
 
-	transition: "fade",
+	mounted() {
+		this.scrollEvent();
+	},
 
 	watch: {
 		loading(now, _old) {
