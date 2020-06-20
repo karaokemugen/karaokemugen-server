697afcf2ce69a1580938fd6b6d3f0e73f5ebf453
Guillaume Lebigot <axel@teri-chan.net>
2020-06-20 23:43:19 +0200

    I have no idea how this worked before O_O

--- src/services/tag.ts
+++ src/services/tag.ts
@@ -14,7 +14,7 @@ export function formatTagList(tagList: DBTag[], from: number, count: number): Ta
 		infos: {
 			count: count,
 			from: +from,
-			to: +from + +tagList.length
+			to: +from + +count
 		},
 		content: tagList
 	};
@@ -23,8 +23,7 @@ export function formatTagList(tagList: DBTag[], from: number, count: number): Ta
 export async function getTags(params: TagParams) {
 	try {
 		const tags = await selectTags(params);
-		return formatTagList(tags.slice(params.from || 0,
-			(params.from || 0) + params.size || 999999999), params.from || 0, tags.length);
+		return formatTagList(tags, +params.from, tags.length);
 	} catch(err) {
 		sentry.addErrorInfo('args', JSON.stringify(arguments, null, 2));
 		sentry.error(err);
