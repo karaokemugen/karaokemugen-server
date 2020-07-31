4d1acaf2257a34dc3295dd72e5358ce57ca090e0
Leon <me@leonekmi.fr>
2020-07-31 15:57:04 +0200

    :lipstick: SearchBar.vue lint

--- kmexplorer/components/SearchBar.vue
+++ kmexplorer/components/SearchBar.vue
@@ -1,22 +1,27 @@
 <template>
 	<div class="field is-expanded has-addons">
 		<div class="control is-expanded">
-			<input class="input is-fullwidth" type="text" :placeholder="$t('search.placeholder')" v-model="search"
-				   @keydown.enter="triggerSearch">
+			<input
+				v-model="search"
+				class="input is-fullwidth"
+				type="text"
+				:placeholder="$t('search.placeholder')"
+				@keydown.enter="triggerSearch"
+			>
 		</div>
 		<div class="control">
 			<span class="select">
-				<select :aria-label="$t('search.aria.sort')" v-model="sort">
+				<select v-model="sort" :aria-label="$t('search.aria.sort')">
 					<option value="az" selected>{{ $t('search.sort.a_z') }}</option>
-                    <template v-if="['types-id', 'years-year'].includes(this.$route.name)">
-					    <option value="karacount">{{ $t('search.sort.kara_count') }}</option>
-                    </template>
-                    <template v-else>
-                        <option value="recent">{{ $t('search.sort.recent') }}</option>
-                        <option value="most_played">{{ $t('search.sort.most_played') }}</option>
-                        <option value="most_favorites">{{ $t('search.sort.most_favorites') }}</option>
-                        <option value="most_requested">{{ $t('search.sort.most_requested') }}</option>
-                    </template>
+					<template v-if="['types-id', 'years-year'].includes(this.$route.name)">
+						<option value="karacount">{{ $t('search.sort.kara_count') }}</option>
+					</template>
+					<template v-else>
+						<option value="recent">{{ $t('search.sort.recent') }}</option>
+						<option value="most_played">{{ $t('search.sort.most_played') }}</option>
+						<option value="most_favorites">{{ $t('search.sort.most_favorites') }}</option>
+						<option value="most_requested">{{ $t('search.sort.most_requested') }}</option>
+					</template>
 				</select>
 			</span>
 		</div>
@@ -25,7 +30,7 @@
 
 <script lang="ts">
 	import Vue from 'vue';
-	import {menuBarStore} from "~/store";
+	import { menuBarStore } from '~/store';
 
 	interface VState {
 		search: string,
@@ -34,19 +39,13 @@
 	}
 
 	export default Vue.extend({
-		name: "SearchBar",
+		name: 'SearchBar',
 
 		data(): VState {
 			return {
 				search: '',
 				sort: 'az'
-			}
-		},
-
-		methods: {
-			triggerSearch() {
-				this.$router.push(`/search/${this.search}`);
-			}
+			};
 		},
 
 		watch: {
@@ -69,7 +68,13 @@
 		},
 
 		destroyed() {
-			if (this.VuexUnsubscribe) this.VuexUnsubscribe();
+			if (this.VuexUnsubscribe) { this.VuexUnsubscribe(); }
+		},
+
+		methods: {
+			triggerSearch() {
+				this.$router.push(`/search/${this.search}`);
+			}
 		}
 	});
 </script>
