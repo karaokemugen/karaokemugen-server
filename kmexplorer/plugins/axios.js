79c9272055df3183ce89e869ee2614f21aa43fc6
Aeden <florentius.b@gmail.com>
2020-08-29 12:35:05 +0200

    Fix KM-SERVER-2J

--- kmexplorer/plugins/axios.js
+++ kmexplorer/plugins/axios.js
@@ -1,11 +1,11 @@
 export default function ({ $axios, app }) {
 	$axios.onResponse((res) => {
-		if (res.data.code) { // ?
+		if (res?.data?.code) { // if no code is present don't display toast
 			app.$toast.success(app.i18n.t(`toast.${res.data.code}`));
 		}
 	});
 	$axios.onError((err) => {
-		if (err?.response.data?.code) { // ?
+		if (err?.response.data?.code) { // if no code is present don't display toast
 			app.$toast.error(app.i18n.t(`toast.${err.response.data.code}`), { icon: 'error' });
 		}
 	});
