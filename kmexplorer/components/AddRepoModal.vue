8cc4a878b65e45d1c1e5bb1ad51767c53fcdd1b1
Aeden <florentius.b@gmail.com>
2020-09-15 23:56:36 +0200

    Add generic component for Modal

--- kmexplorer/components/AddRepoModal.vue
+++ kmexplorer/components/AddRepoModal.vue
@@ -1,54 +1,36 @@
 <template>
-	<div class="modal" :class="{'is-active': active}">
-		<form action="#" @submit.prevent="submitForm">
-			<div class="modal-background" @click="closeModal" />
-			<div class="modal-card">
-				<header>
-					<div class="modal-card-head is-flex">
-						{{ $t('modal.add_repository.label') }}
-						<div><a class="delete" aria-label="close" @click="closeModal" /></div>
-					</div>
-				</header>
-				<section class="modal-card-body">
-					<label class="label">
-						{{ $t('modal.add_repository.desc') }}
-						<a :href="`https://mugen.karaokes.moe/${$i18n.locale === 'fre' ? '' : 'en/'}download.html`">
-							{{ $t('modal.add_repository.download') }}
-						</a>
-					</label>
-					<label class="label">
-						<i18n path="modal.add_repository.manual" tag="label">
-							<template v-slot:repository>
-								<span class="boldLabel">{{ explorerHost }}</span>
-							</template>
-							<template v-slot:online>
-								<span class="boldLabel">{{ $t('modal.add_repository.online') }}</span>
-							</template>
-						</i18n>
-					</label>
-				</section>
-				<footer class="modal-card-foot">
-					<button
-						class="button is-success"
-						type="submit"
-					>
-						{{ $t('modal.add_repository.add') }}
-					</button>
-					<button
-						type="button"
-						class="button"
-						@click="closeModal"
-					>
-						{{ $t('modal.add_repository.cancel') }}
-					</button>
-				</footer>
-			</div>
-		</form>
-	</div>
+	<modal
+		:active="active"
+		:modal-title="$t('modal.add_repository.label')"
+		:submit-action="submitForm"
+		:close="closeModal"
+		:submit-label="$t('modal.add_repository.add')"
+		:cancel-label="$t('modal.add_repository.cancel')"
+	>
+		<section class="modal-card-body">
+			<label class="label">
+				{{ $t('modal.add_repository.desc') }}
+				<a :href="`https://mugen.karaokes.moe/${$i18n.locale === 'fre' ? '' : 'en/'}download.html`">
+					{{ $t('modal.add_repository.download') }}
+				</a>
+			</label>
+			<label class="label">
+				<i18n path="modal.add_repository.manual" tag="label">
+					<template v-slot:repository>
+						<span class="boldLabel">{{ explorerHost }}</span>
+					</template>
+					<template v-slot:online>
+						<span class="boldLabel">{{ $t('modal.add_repository.online') }}</span>
+					</template>
+				</i18n>
+			</label>
+		</section>
+	</modal>
 </template>
 
 <script lang="ts">
 	import Vue from 'vue';
+	import Modal from '~/components/Modal.vue';
 
 	interface VState {
 		explorerHost?: string
@@ -56,6 +38,10 @@
 
 	export default Vue.extend({
 		name: 'AddRepoModal',
+
+		components: {
+			Modal
+		},
 
 		props: {
 			active: Boolean
@@ -83,10 +69,6 @@
 
 	.label {
 		font-weight: normal;
-	}
-
-	.modal-card-head {
-		justify-content: space-between;
 	}
 
 	.boldLabel {
---
+++ kmexplorer/components/Modal.vue
@@ -0,0 +1,66 @@
+<template>
+	<div class="modal" :class="{'is-active': active}">
+		<form action="#" @submit.prevent="submitForm">
+			<div class="modal-background" @click="closeModal" />
+			<div class="modal-card">
+				<header>
+					<div class="modal-card-head is-flex">
+						{{ modalTitle }}
+						<div><a class="delete" aria-label="close" @click="closeModal" /></div>
+					</div>
+				</header>
+				<slot />
+				<footer class="modal-card-foot">
+					<button
+						class="button is-success"
+						type="submit"
+					>
+						{{ submitLabel }}
+					</button>
+					<button
+						type="button"
+						class="button"
+						@click="closeModal"
+					>
+						{{ cancelLabel }}
+					</button>
+				</footer>
+			</div>
+		</form>
+	</div>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+
+	export default Vue.extend({
+		name: 'Modal',
+
+		props: {
+			active: Boolean,
+			modalTitle: String,
+			submitAction: Function,
+			close: Function,
+			submitLabel: String,
+			cancelLabel: String
+		},
+
+		methods: {
+			submitForm(): void {
+				this.submitAction();
+				this.closeModal();
+			},
+			closeModal(): void {
+				this.close();
+			}
+		}
+	});
+</script>
+
+<style lang="scss" scoped>
+
+	.modal-card-head {
+		justify-content: space-between;
+	}
+
+</style>
