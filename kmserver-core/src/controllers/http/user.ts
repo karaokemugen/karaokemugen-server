cdbac42d6113f9710787c7bb9132684ebfc4dded
Guillaume Lebigot <axel@teri-chan.net>
2020-11-21 17:18:03 +0100

    Forgot something

--- kmserver-core/src/controllers/http/user.ts
+++ kmserver-core/src/controllers/http/user.ts
@@ -200,7 +200,7 @@ export default function userController(router: Router) {
  * @apiErrorExample Error-Response:
  * HTTP/1.1 403 Forbidden
  */
-		.put(upload.single('avatarfile'), requireAuth, requireValidUser, updateLoginTime, async (req: any, res: any) => {
+		.put(requireAuth, requireValidUser, updateLoginTime, async (req: any, res: any) => {
 			const validationErrors = check(req.body, {
 				nickname: {presence: true}
 			});
