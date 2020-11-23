682ae85d8302a5f198d0514153c519496957ca7f
Leon <me@leonekmi.fr>
2020-11-23 19:07:24 +0100

    :bug: Change return signature

--- kmserver-core/src/controllers/ws/remote.ts
+++ kmserver-core/src/controllers/ws/remote.ts
@@ -2,12 +2,12 @@ import { SocketIOApp } from '../../lib/utils/ws';
 import logger from '../../lib/utils/logger';
 import { APIData } from '../../lib/types/api';
 import { RemoteResponse, RemoteSettings } from '../../types/remote';
-import {proxyBroadcast, startRemote, stopRemote} from '../../services/remote';
+import { proxyBroadcast, startRemote, stopRemote } from '../../services/remote';
 
 export default function remoteSocketController(app: SocketIOApp) {
-	app.route('remote start', async (socket, req: APIData<RemoteSettings>): Promise<{body: RemoteResponse}> => {
+	app.route('remote start', async (socket, req: APIData<RemoteSettings>): Promise<RemoteResponse> => {
 		logger.info(`Start remote for ${req.body.InstanceID}`, {service: 'Remote'});
-		return {body: startRemote(socket, req.body.InstanceID)};
+		return startRemote(socket, req.body.InstanceID);
 	});
 	app.route('remote broadcast', async (socket, req: APIData) => {
 		proxyBroadcast(socket, req.body);
