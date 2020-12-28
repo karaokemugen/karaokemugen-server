dc7dbb67aed30375c51dd6b3607750ddb1a67218
Leon <me@leonekmi.fr>
2020-12-29 00:24:51 +0100

    :wrench: Include user data in user updated events.

--- kmserver-core/src/services/user_pubsub.ts
+++ kmserver-core/src/services/user_pubsub.ts
@@ -1,6 +1,8 @@
 import { Socket } from 'socket.io';
 import { getWS } from '../lib/utils/ws';
+import logger from '../lib/utils/logger';
 import { findUserByName } from './user';
+import {getFavorites} from './favorites';
 
 export async function subUser(socket: Socket, username: string) {
 	if (await findUserByName(username)) {
@@ -16,8 +18,17 @@ export function unsubUser(socket: Socket, username: string) {
 	return true;
 }
 
-export function pubUser(username: string) {
-	getWS().ws.to(username).emit('user updated', username);
+export async function pubUser(username: string) {
+	const [user, favorites] = await Promise.all([
+		findUserByName(username, { public: true, password: false }),
+		getFavorites({ username, role: 'user' })
+	]);
+	if (user === false) {
+		// wtf?
+		logger.warn('A unknown user was pubbed, not normal', { service: 'UserPubsub', obj: username });
+		return;
+	}
+	getWS().ws.to(username).emit('user updated', { user, favorites });
 }
 
 export function delPubUser(username: string) {
