9834da957217326327530f19c5f48445837c26a9
Guillaume Lebigot <axel@teri-chan.net>
2021-04-18 18:53:06 +0200

    Migrations

---
+++ kmserver-core/migrations/20210418000000.do.addPlaylists.sql
@@ -0,0 +1,50 @@
+CREATE TABLE playlists (
+	pk_plaid UUID PRIMARY KEY,
+	name CHARACTER VARYING NOT NULL,
+	slug CHARACTER VARYING UNIQUE NOT NULL,
+	karacount INTEGER NOT NULL,
+	duration INTEGER NOT NULL,
+	created_at TIMESTAMPTZ NOT NULL,
+	modified_at TIMESTAMPTZ NOT NULL,
+	flag_visible BOOLEAN DEFAULT TRUE,
+	flag_visible_online BOOLEAN DEFAULT TRUE,
+	fk_login CHARACTER VARYING NOT NULL,
+	fk_id_plcontent_playing INTEGER,
+	FOREIGN KEY(fk_login)
+	  REFERENCES users(pk_login)
+	  ON DELETE CASCADE
+	  ON UPDATE CASCADE
+);
+
+CREATE INDEX idx_playlists_slug ON playlists(slug);
+
+CREATE TABLE playlists_contributors (
+	fk_plaid UUID,
+	fk_login CHARACTER VARYING,
+	FOREIGN KEY(fk_plaid)
+	  REFERENCES playlists(pk_plaid)
+	  ON DELETE CASCADE
+	  ON UPDATE CASCADE,
+	FOREIGN KEY(fk_login)
+	  REFERENCES users(pk_login)
+	  ON DELETE CASCADE
+	  ON UPDATE CASCADE
+);
+
+CREATE TABLE playlists_content(
+	pk_plc_id SERIAL PRIMARY KEY,
+	fk_plaid UUID NOT NULL,
+	fk_kid UUID NOT NULL,
+	created_at TIMESTAMPTZ NOT NULL,
+	pos INTEGER NOT NULL,
+	nickname CHARACTER VARYING,
+	fk_login CHARACTER VARYING NOT NULL,
+	flag_free BOOLEAN DEFAULT TRUE,
+	flag_visible BOOLEAN DEFAULT TRUE,
+	flag_accepted BOOLEAN DEFAULT FALSE,
+	flag_refused BOOLEAN DEFAULT FALSE,
+	FOREIGN KEY(fk_plaid)
+	  REFERENCES playlists(pk_plaid)
+	  ON DELETE CASCADE
+	  ON UPDATE CASCADE
+)
