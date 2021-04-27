e225ecfeaf52ce4e844d6847690c626c2be2c8c7
Aeden <florentius.b@gmail.com>
2021-04-27 17:30:00 +0200

    🚨 Fix Vetur warning

--- kmexplorer/components/KaraFullInfo.vue
+++ kmexplorer/components/KaraFullInfo.vue
@@ -46,9 +46,6 @@
 				</tr>
 			</tbody>
 		</table>
-		<!-- <div class="content">
-			{{ $t('kara.duration') }}:&nbsp;{{ duration }}&nbsp;/&nbsp;{{ $t('kara.created_at') }}:&nbsp;{{ new Date(karaoke.created_at).toLocaleString() }}&nbsp;/&nbsp;{{ $t('kara.modified_at') }}:&nbsp;{{ new Date(karaoke.modified_at).toLocaleString() }}
-		</div> -->
 		<div class="buttons">
 			<button class="button is-info" @click="toggleLyrics">
 				<font-awesome-icon :icon="['fas', 'closed-captioning']" :fixed-width="true" />
