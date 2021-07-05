0c4792e14dfc8a6aedfc6c8a72856f99a0a64722
Leon <me@leonekmi.fr>
2021-07-05 23:35:45 +0200

    🐛 Fix migration Axel mistakes.

--- kmserver-core/migrations/20210705000001.do.addPublicProfiles.sql
+++ kmserver-core/migrations/20210705000001.do.addPublicProfiles.sql
@@ -1,5 +1,5 @@
 ALTER TABLE users ADD COLUMN flag_public BOOLEAN DEFAULT(true);
-ALTER TABLE users ADD COLUMN flag_diplayfavorites BOOLEAN DEFAULT(false);
+ALTER TABLE users ADD COLUMN flag_displayfavorites BOOLEAN DEFAULT(false);
 ALTER TABLE users ADD COLUMN social_networks JSONB;
 ALTER TABLE users ADD COLUMN banner CHARACTER VARYING;
 
