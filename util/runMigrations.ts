348c69511795a49b6a8516c52db8fdf32b3e1cf6
Guillaume Lebigot <axel@teri-chan.net>
2021-10-16 00:30:14 +0200

    🚑 Fix run migrations script

--- util/runMigrations.ts
+++ util/runMigrations.ts
@@ -1,24 +1,32 @@
 import {load} from 'js-yaml';
 import {readFileSync} from 'fs';
 import Postgrator from 'postgrator';
+import { Client } from 'pg';
 
 async function migrate() {
 	const ymlConfig = readFileSync('../app/config.yml', 'utf-8');
 	const conf: any = load(ymlConfig);
+	const dbConfig = {
+		host: conf.System.Database.host,
+		user: conf.System.Database.username,
+		port: conf.System.Database.port,
+		password: conf.System.Database.password,
+		database: conf.System.Database.database
+	};
+	const client = new Client(dbConfig)
+	await client.connect()
 	const migrator = new Postgrator({
 		migrationPattern: 'migrations/*.sql',
-		host: conf.System.Database.host,
 		driver: 'pg',
-		username: conf.System.Database.username,
-		password: conf.System.Database.password,
-		port: conf.System.Database.port,
 		database: conf.System.Database.database,
+		execQuery: (query) => client.query(query),
 		validateChecksums: false,
 });
 	const migrations = await migrator.migrate();
 	migrations.length > 0
 		? console.log(`Executed ${migrations.length} migrations`)
 		: console.log('No migrations to execute');
+	await client.end();
 }
 
 migrate().catch(err => console.log(err));
