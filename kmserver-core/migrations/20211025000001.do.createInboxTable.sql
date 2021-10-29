064be97900232e9521f30ba06b15b71f7672ad00
Guillaume Lebigot <axel@teri-chan.net>
2021-10-29 14:12:37 +0200

    🚑 Fix migration

--- kmserver-core/migrations/20211025000001.do.createInboxTable.sql
+++ kmserver-core/migrations/20211025000001.do.createInboxTable.sql
@@ -1,5 +1,5 @@
 CREATE TABLE inbox (
-	pk_kid PRIMARY KEY UUID,
+	pk_kid UUID NOT NULL,
 	name TEXT,
 	fk_login_downloaded TEXT,
 	downloaded_at TIMESTAMPTZ,
@@ -10,6 +10,7 @@ CREATE TABLE inbox (
 	mediafile TEXT,
 	gitlab_issue INTEGER,
 	fix BOOLEAN,
+	CONSTRAINT inbox_kid_pkey PRIMARY KEY (pk_kid)
 )
 
 ALTER TABLE inbox ADD CONSTRAINT inbox_fk_login_downloaded_fkey FOREIGN KEY (fk_login_downloaded) REFERENCES users(pk_login) ON DELETE CASCADE;
