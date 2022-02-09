bef67bd6c3ae175d57bf5b358753fa3a079b5f30
Leon <me@leonekmi.fr>
2022-02-09 20:27:40 +0000

    Fix migration blblbl

--- kmserver-core/migrations/20220118171735.do.refactorInbox.sql
+++ kmserver-core/migrations/20220118171735.do.refactorInbox.sql
@@ -10,10 +10,6 @@ ALTER TABLE inbox
 	DROP COLUMN fix;
 
 ALTER TABLE inbox
-	ADD fk_kid UUID NOT NULL;
+	ADD fk_kid UUID;
 ALTER TABLE inbox
 	ADD edited_kid UUID;
-
--- set existing imports to random KIDs
-UPDATE inbox SET fk_kid = gen_random_uuid();
-
