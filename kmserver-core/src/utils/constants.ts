c297a3cc1995bf1f837a2926e6d1ac3a9814dd2b
Guillaume Lebigot <axel@teri-chan.net>
2022-09-06 15:27:50 +0200

    :fire: Removed unused constant

--- kmserver-core/src/utils/constants.ts
+++ kmserver-core/src/utils/constants.ts
@@ -1,10 +1,5 @@
 import { JWTTokenWithRoles } from '../lib/types/user';
 
-// Default headers for HTTP client
-export const headers = {
-	'user-agent': 'KaraokeMugenServer'
-};
-
 export const sentryDSN = 'https://df475c5a360e4e028798b51cdb38795c@o399537.ingest.sentry.io/5258229';
 
 export const userAgent = 'KaraokeMugenServer';
