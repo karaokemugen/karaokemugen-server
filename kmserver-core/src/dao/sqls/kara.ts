8bd5240ba7648e8f573171c6d2f9d718c2667487
Guillaume Lebigot <axel@teri-chan.net>
2022-10-24 11:32:59 +0200

    Added anime list IDs to getKaras query and type

--- kmserver-core/src/dao/sqls/kara.ts
+++ kmserver-core/src/dao/sqls/kara.ts
@@ -46,6 +46,9 @@ SELECT
   ak.repository AS repository,
   ak.comment AS comment,
   ak.ignore_hooks AS ignore_hooks,
+  ak.anilist_ids AS anilist_ids,
+  ak.myanimelist_ids AS myanimelist_ids,
+  ak.kitsu_ids AS kitsu_ids,
   ksub.subchecksum AS subchecksum,
   ak.pk_kid || '.' || ak.mediasize::text || '.' || COALESCE(ksub.subchecksum, 'no_ass_file') || '.mp4' AS hardsubbed_mediafile,
   ${selectClause}
@@ -68,7 +71,7 @@ WHERE ${includeStaging ? '1 = 1' : 'ak.repository != \'Staging\''}
 	}
 	${filterClauses.map(clause => `AND (${clause})`).reduce((a, b) => (`${a} ${b}`), '')}
 	${whereClauses}
-GROUP BY ${groupClause} ak.pk_kid, ak.titles, ak.titles_aliases, ak.titles_default_language, ak.songorder, ak.tags, ak.serie_singergroup_singer_sortable, ak.subfile, ak.year, ak.mediafile, ak.karafile, ak.duration, ak.gain, ak.loudnorm, ak.created_at, ak.modified_at, ak.mediasize, ak.repository, ak.comment, ak.songtypes_sortable, ak.ignore_hooks, ak.titles_sortable, ksub.subchecksum
+GROUP BY ${groupClause} ak.pk_kid, ak.titles, ak.titles_aliases, ak.titles_default_language, ak.songorder, ak.tags, ak.serie_singergroup_singer_sortable, ak.subfile, ak.year, ak.mediafile, ak.karafile, ak.duration, ak.gain, ak.loudnorm, ak.created_at, ak.modified_at, ak.mediasize, ak.repository, ak.comment, ak.songtypes_sortable, ak.ignore_hooks, ak.titles_sortable, ksub.subchecksum, ak.kitsu_ids, ak.anilist_ids, ak.myanimelist_ids
 ORDER BY ${orderClauses} ak.serie_singergroup_singer_sortable, ak.songtypes_sortable DESC, ak.songorder, ak.titles_sortable
 ${limitClause}
 ${offsetClause}
