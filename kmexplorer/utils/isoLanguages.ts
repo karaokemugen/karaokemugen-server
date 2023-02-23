060e1be80cd4ef9fc9ebd6928a37d3793f2be664
TheMio <themio@protonmail.ch>
2023-02-23 14:07:15 +0100

    🐛 Show all languages in selection

--- kmexplorer/utils/isoLanguages.ts
+++ kmexplorer/utils/isoLanguages.ts
@@ -1,8 +1,8 @@
-import { registerLocale, getNames, getAlpha3BCode, getName, alpha2ToAlpha3B } from '@karaokemugen/i18n-iso-languages';
+import { alpha2ToAlpha3B, getAlpha3BCodes, getName, registerLocale } from '@karaokemugen/i18n-iso-languages';
+import de from '@karaokemugen/i18n-iso-languages/langs/de.json';
 import en from '@karaokemugen/i18n-iso-languages/langs/en.json';
 import fr from '@karaokemugen/i18n-iso-languages/langs/fr.json';
 import id from '@karaokemugen/i18n-iso-languages/langs/id.json';
-import de from '@karaokemugen/i18n-iso-languages/langs/de.json';
 import pt from '@karaokemugen/i18n-iso-languages/langs/pt.json';
 
 registerLocale(en);
@@ -59,14 +59,16 @@ export const langWithRomanization = [
 ];
 
 export function getListLanguagesInLocale(userLang:string): Array<{ value: string, label: string }> {
-	const result = [];
 	if (process.client) {
-		const langs = Object.values(getNames(userLang));
-		for (const langInLocale of langs) {
-			result.push({ value: getAlpha3BCode(langInLocale, userLang) as string, label: langInLocale });
-		}
+		const langMap = getAlpha3BCodes();
+		const langs = Object.keys(langMap).map((lang3BCode) => ({
+			value: lang3BCode,
+			label: getName(lang3BCode, userLang) 
+				|| langMap[lang3BCode], // fallback to primary language
+		}));
+		return langs;
 	}
-	return result;
+	return [];
 }
 
 export function getLanguagesInLocaleFromCode(code: string, userLang:string) {
