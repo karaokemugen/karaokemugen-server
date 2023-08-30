12a4fe2c6c977a1da15d8472cc433b88fd1f248c
Guillaume Lebigot <axel@teri-chan.net>
2023-08-30 15:00:45 +0200

    :bug: Fix mes conneries

--- kmserver-core/src/types/database/playlist.d.ts
+++ kmserver-core/src/types/database/playlist.d.ts
@@ -1,8 +1,5 @@
 import { DBPLBase } from '../../lib/types/database/playlist.js';
 
 export interface DBPL extends DBPLBase {
-    description?: string;
-    slug?: string;
-    contributors?: string[];
-    flag_visible: boolean;
+        
 }
