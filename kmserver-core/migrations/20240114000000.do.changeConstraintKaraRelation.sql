bdf26c90a01e9bbdc3d16330fffd927ac39bb346
Guillaume Lebigot <axel@teri-chan.net>
2024-01-14 11:22:24 +0100

    🐛 Change constraint on kara_relation to allow delete cascade

---
+++ kmserver-core/migrations/20240114000000.do.changeConstraintKaraRelation.sql
@@ -0,0 +1,9 @@
+ALTER TABLE kara_relation DROP CONSTRAINT IF EXISTS kara_relation_fk_kid_child_fkey;
+
+ALTER TABLE kara_relation ADD CONSTRAINT kara_relation_fk_kid_child_fkey
+ FOREIGN KEY (fk_kid_child) REFERENCES kara(pk_kid) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE kara_relation DROP CONSTRAINT IF EXISTS kara_relation_fk_kid_parent_fkey;
+
+ALTER TABLE kara_relation ADD CONSTRAINT kara_relation_fk_kid_parent_fkey
+ FOREIGN KEY (fk_kid_parent) REFERENCES kara(pk_kid) ON UPDATE CASCADE ON DELETE CASCADE;
