e65542b305e7fe703b8ff9def9db5cdd1490f84b
Florent Berthelot <florentius.b@gmail.com>
2024-07-06 13:51:48 +0200

    ♻️🥅 Override init for SentryLogger to ignore errors

--- kmserver-core/src/utils/sentry.ts
+++ kmserver-core/src/utils/sentry.ts
@@ -2,9 +2,43 @@ import * as SentryNode from '@sentry/node';
 import Transport from 'winston-transport';
 
 import SentryLogger from '../lib/utils/sentry.js';
+import { getState } from './state.js';
+import { getConfig } from '../lib/utils/config.js';
 
-const Sentry = new SentryLogger(SentryNode);
+class NodeSentryLogger extends SentryLogger {
+	init() {
+		if (process.env.CI_SERVER || process.env.SENTRY_TEST) {
+			console.log('CI detected/SENTRY_TEST present - Sentry disabled');
+			console.log("Have a nice day, sentries won't fire at you~");
+			return;
+		}
+		if (!process.env.SENTRY_DSN) {
+			// No DSN provided, return.
+			return;
+		}
+		this.Sentry.init({
+			dsn: process.env.SENTRY_DSN,
+			environment: process.env.SENTRY_ENVIRONMENT || 'release',
+			release: getState().version.number,
+			dist: getState().version.sha,
+			ignoreErrors: [
+				'[nuxt] instance unavailable'
+			],
+			beforeSend: (event, _hint) => {
+				// Testing for precise falseness. If errortracking is undefined or if getconfig doesn't return anything, errors are not sent.
+				if (
+					getConfig()?.Online?.ErrorTracking !== true ||
+					!this.SentryInitialized
+				)
+					return null;
+				return event;
+			},
+		});
+		this.SentryInitialized = true;
+	}
+}
 
+const Sentry = new NodeSentryLogger(SentryNode);
 export default Sentry;
 
 export class SentryTransport extends Transport {
