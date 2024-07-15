df87a2d48d9b04f5fe27602dd74d01280dedb17f
Florent Berthelot <florentius.b@gmail.com>
2024-07-15 23:27:37 +0200

    🐛 Fix init value for from_display_type in kara form

--- kmexplorer/utils/kara.ts
+++ kmexplorer/utils/kara.ts
@@ -95,6 +95,7 @@ export function convertDBKaraToKaraFile(dbKara?: DBKara): KaraFileV4 {
 							return [t, []];
 						}
 					})),
+			from_display_type: dbKara?.from_display_type,
 			titles: dbKara?.titles || {},
 			titles_default_language: dbKara?.titles_default_language,
 			titles_aliases: dbKara?.titles_aliases || [],
