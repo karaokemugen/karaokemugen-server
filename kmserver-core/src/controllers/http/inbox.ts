624fd51a46b8ed84f99eca7088fee100c46a03ec
Guillaume Lebigot <axel@teri-chan.net>
2024-07-23 10:56:00 +0200

    :sparkles: Added route to assign issue to user

--- kmserver-core/src/controllers/http/inbox.ts
+++ kmserver-core/src/controllers/http/inbox.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { APIMessage } from '../../lib/services/frontend.js';
 import { getInbox, getKaraInbox, markKaraInboxAsDownloaded, removeKaraFromInbox } from '../../services/inbox.js';
 import {requireAuth, requireMaintainer, requireValidUser, updateLoginTime} from '../middlewares/auth.js';
+import { assignIssue } from '../../lib/utils/gitlab.js';
 
 export default function inboxController(router: Router) {
 	router.route('/inbox/:inid([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})')
@@ -26,6 +27,15 @@ export default function inboxController(router: Router) {
 		.post(requireAuth, requireValidUser, requireMaintainer, updateLoginTime, async (req: any, res) => {
 			try {
 				await markKaraInboxAsDownloaded(req.params.inid, req.authToken.username);
+				res.status(200).json();
+			} catch (err) {
+				res.status(err.code || 500).json(APIMessage(err.message));
+			}
+		});
+	router.route('/inbox/:inid([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/assignToUser')
+		.post(requireAuth, requireValidUser, requireMaintainer, updateLoginTime, async (req: any, res) => {
+			try {
+				await assignIssue(req.body.issue, req.body.repoName, req.body.gitlabUsername)
 				res.status(200).json();
 			} catch (err) {
 				res.status(err.code || 500).json(APIMessage(err.message));
