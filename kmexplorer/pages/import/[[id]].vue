524fa0d2afed6747ed1244b5aa05f3b0d54e4c83
Florent Berthelot <florentius.b@gmail.com>
2025-06-09 22:10:56 +0200

    🐛 Fix import page display

--- kmexplorer/pages/import/[[id]].vue
+++ kmexplorer/pages/import/[[id]].vue
@@ -72,7 +72,7 @@
 	const kara = ref<DBKara>();
 	const manifest = ref<RepositoryManifestV2>();
 
-	if (!config?.value?.Frontend.Import.Enabled) {
+	if (config?.value && !config.value.Frontend.Import.Enabled) {
 		throw createError({
 			statusCode: 404,
 			statusMessage: 'Page Not Found'
