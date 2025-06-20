0e40d544fd3d2d5e844ceeff6d82abddfd82674e
Michel Roux <xefir@crystalyx.net>
2025-06-20 16:47:43 +0200

    🔥Remove useless gitpod feature

--- .gitpod/cloneKaraokebase.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-mkdir -p "$GITPOD_REPO_ROOT/app/data/git"
-git clone --depth=1 ${karaokebase_url:-$default_karaokebase_url}.git ${KARAOKEBASE_BRANCH:+--branch $KARAOKEBASE_BRANCH} "$GITPOD_REPO_ROOT/app/data/git"
--- .gitpod/setupApp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-yarn pull
-yarn install
-yarn build:all
-yarn migrate
-yarn start --generate > app/generate.log 2>&1 &
-      
