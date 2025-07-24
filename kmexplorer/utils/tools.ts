c67e009d792b16b2fdbef7a3ef81b4cd88d0d45a
Florent Berthelot <florentius.b@gmail.com>
2025-07-24 23:08:02 +0200

    🌐 Fallback to browser language for tags

--- kmexplorer/utils/tools.ts
+++ kmexplorer/utils/tools.ts
@@ -8,25 +8,44 @@ import { useAuthStore } from '~/store/auth';
 import { storeToRefs } from 'pinia';
 import slug from 'slug';
 
-export function getPropertyInLanguage(prop: 'i18n', tag: DBKaraTag | DBTag, mainLanguage: string, fallbackLanguage: string, i18nParam?: Record<string, string>): string
-export function getPropertyInLanguage(prop: 'description', tag: DBTag, mainLanguage: string, fallbackLanguage: string): string | null
-export function getPropertyInLanguage(prop: 'description' | 'i18n', tag: DBKaraTag | DBTag, mainLanguage: string, fallbackLanguage: string, i18nParam?: Record<string, string>): string | null {
+export function getPropertyInLanguage(
+	prop: 'i18n',
+	tag: DBKaraTag | DBTag,
+	mainLanguage: string,
+	fallbackLanguage: string,
+	i18nParam?: Record<string, string>,
+): string;
+export function getPropertyInLanguage(
+	prop: 'description',
+	tag: DBTag,
+	mainLanguage: string,
+	fallbackLanguage: string,
+): string | null;
+export function getPropertyInLanguage(
+	prop: 'description' | 'i18n',
+	tag: DBKaraTag | DBTag,
+	mainLanguage: string,
+	fallbackLanguage: string,
+	i18nParam?: Record<string, string>,
+): string | null {
 	const i18n = i18nParam ? i18nParam : tag[prop];
 	if (i18n) {
 		return i18n[mainLanguage]
 			? i18n[mainLanguage]
-			: (i18n[fallbackLanguage]
+			: i18n[fallbackLanguage]
 				? i18n[fallbackLanguage]
-				: (i18n.eng ? i18n.eng : 
-					(prop === 'description' ? null : tag.name)
-				)
-			);
+				: i18n.eng
+					? i18n.eng
+					: prop === 'description'
+						? null
+						: tag.name;
 	} else {
 		return prop === 'i18n' ? tag.name : '';
 	}
 }
 
 export function getTagInLocale(tag: DBKaraTag | DBTag, i18nParam?: Record<string, string>) {
+	const nuxt = useNuxtApp();
 	const { user } = storeToRefs(useAuthStore());
 	const tagLang =
 		(tag as DBKaraTag).type_in_kara || (tag as DBTag).types
@@ -34,7 +53,13 @@ export function getTagInLocale(tag: DBKaraTag | DBTag, i18nParam?: Record<string
 					.language
 			: null;
 	if (tagLang === 'user' && user?.value && user.value.language) {
-		return getPropertyInLanguage('i18n', tag, getLanguageIn3B(user.value.language), 'eng', i18nParam);
+		return getPropertyInLanguage(
+			'i18n',
+			tag,
+			getLanguageIn3B(user.value.language),
+			getBrowserLanguageIn3B(),
+			i18nParam,
+		);
 	} else if (
 		tagLang === 'song_name' &&
 		user?.value &&
@@ -49,73 +74,85 @@ export function getTagInLocale(tag: DBKaraTag | DBTag, i18nParam?: Record<string
 			i18nParam,
 		);
 	} else {
-		return getPropertyInLanguage('i18n', tag, getBrowserLanguageIn3B(), 'eng', i18nParam);
+		return getPropertyInLanguage('i18n', tag, getLanguageIn3B(nuxt.$i18n.locale.value), getBrowserLanguageIn3B(), i18nParam);
 	}
 }
 
 export function getDescriptionInLocale(tag: DBTag) {
+	const nuxt = useNuxtApp();
 	const { user } = storeToRefs(useAuthStore());
 	if (user?.value && user?.value.language) {
-		return getPropertyInLanguage('description', tag, getLanguageIn3B(user.value.language), 'eng');
+		return getPropertyInLanguage(
+			'description',
+			tag,
+			getLanguageIn3B(user.value.language),
+			getBrowserLanguageIn3B(),
+		);
 	} else {
-		return getPropertyInLanguage('description', tag, getBrowserLanguageIn3B(), 'eng');
+		return getPropertyInLanguage('description', tag, getLanguageIn3B(nuxt.$i18n.locale.value), getBrowserLanguageIn3B());
 	}
 }
 
-export function getTitleInLocale(titles: Record<string, string>, titles_default_language?:string) {
+export function getTitleInLocale(titles: Record<string, string>, titles_default_language?: string) {
+	const nuxt = useNuxtApp();
 	const { user } = storeToRefs(useAuthStore());
 	if (user?.value && user?.value.main_series_lang && user?.value.fallback_series_lang) {
 		return titles[user?.value.main_series_lang]
 			? titles[user?.value.main_series_lang]
-			: (titles[user?.value.fallback_series_lang]
+			: titles[user?.value.fallback_series_lang]
 				? titles[user?.value.fallback_series_lang]
-				: titles[titles_default_language || 'eng']
-			);
+				: titles[titles_default_language || getBrowserLanguageIn3B()];
 	} else {
-		return titles[getBrowserLanguageIn3B()] ? titles[getBrowserLanguageIn3B()] : titles[titles_default_language || 'eng'];
+		return titles[getLanguageIn3B(nuxt.$i18n.locale.value)]
+			? titles[getLanguageIn3B(nuxt.$i18n.locale.value)]
+			: titles[getBrowserLanguageIn3B()]
+				? titles[getBrowserLanguageIn3B()]
+				: titles[titles_default_language || 'eng'];
 	}
 }
 
 export function buildKaraTitle(
 	data: DBKara,
 	i18nParam?: Record<string, Record<string, string>>,
-	withoutLangs = false
+	withoutLangs = false,
 ): string {
 	const serieText =
-			data.from_display_type && data[data.from_display_type]
-				? data[data.from_display_type]
+		data.from_display_type && data[data.from_display_type]
+			? data[data.from_display_type]
 					.slice(0, 3)
-					.map(e => getTagInLocale(e, i18nParam && i18nParam[e.tid]))
+					.map((e) => getTagInLocale(e, i18nParam && i18nParam[e.tid]))
 					.join(', ') + (data[data.from_display_type].length > 3 ? '...' : '')
-				: data?.series?.length > 0
-					? data.series
+			: data?.series?.length > 0
+				? data.series
 						.slice(0, 3)
-						.map(e => getTagInLocale(e, i18nParam && i18nParam[e.tid]))
+						.map((e) => getTagInLocale(e, i18nParam && i18nParam[e.tid]))
 						.join(', ') + (data.series.length > 3 ? '...' : '')
-					: data?.singergroups?.length > 0
-						? data.singergroups
+				: data?.singergroups?.length > 0
+					? data.singergroups
 							.slice(0, 3)
-							.map(e => getTagInLocale(e, i18nParam && i18nParam[e.tid]))
+							.map((e) => getTagInLocale(e, i18nParam && i18nParam[e.tid]))
 							.join(', ') + (data.singergroups.length > 3 ? '...' : '')
-						: data?.singers?.length > 0
-							? data.singers
+					: data?.singers?.length > 0
+						? data.singers
 								.slice(0, 3)
-								.map(e => getTagInLocale(e, i18nParam && i18nParam[e.tid]))
+								.map((e) => getTagInLocale(e, i18nParam && i18nParam[e.tid]))
 								.join(', ') + (data.singers.length > 3 ? '...' : '')
-							: ''; // wtf?
+						: ''; // wtf?
 	const langsText = data?.langs
-		?.map(e => e.name)
+		?.map((e) => e.name)
 		.join(', ')
 		.toUpperCase();
 	const songtypeText = sortAndHideTags(data?.songtypes)
-		.map(e => (e.short ? +e.short : e.name))
+		.map((e) => (e.short ? +e.short : e.name))
 		.join(' ');
 	const songorderText = data?.songorder > 0 ? ' ' + data.songorder : '';
-	const versions = sortAndHideTags(data?.versions).map(t => `[${getTagInLocale(t, i18nParam && i18nParam[t.tid])}]`);
+	const versions = sortAndHideTags(data?.versions).map(
+		(t) => `[${getTagInLocale(t, i18nParam && i18nParam[t.tid])}]`,
+	);
 	const version = versions?.length > 0 ? ` ${versions.join(' ')}` : '';
 	return `${withoutLangs ? '' : `${langsText} - `}${serieText} - ${songtypeText} ${songorderText} - ${getTitleInLocale(
 		data.titles,
-		data.titles_default_language
+		data.titles_default_language,
 	)} ${version}`;
 }
 
@@ -146,15 +183,23 @@ export function fakeYearTag(year: string, count?: number): DBTag {
 		types: [<any>0],
 		i18n: {},
 		karacount: {
-			0: count || 0
-		}
+			0: count || 0,
+		},
 	};
 }
 
-export function generateNavigation(search = useMenubarStore().search, tags = useMenubarStore().tags, page = '/search/', enabledCollections = useLocalStorageStore().enabledCollections) {
+export function generateNavigation(
+	search = useMenubarStore().search,
+	tags = useMenubarStore().tags,
+	page = '/search/',
+	enabledCollections = useLocalStorageStore().enabledCollections,
+) {
 	const navigation = {
 		path: `${page}${encodeURIComponent(search)}`,
-		query: { collections: encodeURIComponent(enabledCollections?.join(':')) } as { collections: string, q?: string }
+		query: { collections: encodeURIComponent(enabledCollections?.join(':')) } as {
+			collections: string;
+			q?: string;
+		},
 	};
 	const criterias: string[] = [];
 	const tagsUpdated: string[] = [];
@@ -176,7 +221,7 @@ export function generateNavigation(search = useMenubarStore().search, tags = use
 
 function uniqueTag(tags: TagExtend[]) {
 	const tids = new Set();
-	return tags.filter(tag => {
+	return tags.filter((tag) => {
 		if (tids.has(tag.tag.tid)) {
 			return false;
 		}
@@ -188,10 +233,18 @@ function uniqueTag(tags: TagExtend[]) {
 export function getDurationString(duration: number, t: any, withSecondes = true) {
 	const durationArray = getArrayDuration(duration);
 	const returnString = [];
-	if (durationArray[0] !== 0) { returnString.push(`${durationArray[0]} ${t('duration.days')}`); }
-	if (durationArray[1] !== 0) { returnString.push(`${durationArray[1]} ${t('duration.hours')}`); }
-	if (durationArray[2] !== 0) { returnString.push(`${durationArray[2]} ${t('duration.minutes')}`); }
-	if (durationArray[3] !== 0 && withSecondes) { returnString.push(`${durationArray[3]} ${t('duration.seconds')}`); }
+	if (durationArray[0] !== 0) {
+		returnString.push(`${durationArray[0]} ${t('duration.days')}`);
+	}
+	if (durationArray[1] !== 0) {
+		returnString.push(`${durationArray[1]} ${t('duration.hours')}`);
+	}
+	if (durationArray[2] !== 0) {
+		returnString.push(`${durationArray[2]} ${t('duration.minutes')}`);
+	}
+	if (durationArray[3] !== 0 && withSecondes) {
+		returnString.push(`${durationArray[3]} ${t('duration.seconds')}`);
+	}
 	return returnString.join(' ');
 }
 
@@ -222,13 +275,23 @@ function getArrayDuration(duration: any) {
 	return [days, hours, minutes, seconds];
 }
 
-export function getSerieOrSingerGroupsOrSingers(karaoke: DBKara, karaokesI18n?: Record<string, Record<string, string>>) {
-	if (karaoke.from_display_type && karaoke[karaoke.from_display_type] && karaoke[karaoke.from_display_type].length > 0) {
+export function getSerieOrSingerGroupsOrSingers(
+	karaoke: DBKara,
+	karaokesI18n?: Record<string, Record<string, string>>,
+) {
+	if (
+		karaoke.from_display_type &&
+		karaoke[karaoke.from_display_type] &&
+		karaoke[karaoke.from_display_type].length > 0
+	) {
 		return {
-			name: getTagInLocale(karaoke[karaoke.from_display_type][0], karaokesI18n && karaokesI18n[karaoke[karaoke.from_display_type][0].tid]),
+			name: getTagInLocale(
+				karaoke[karaoke.from_display_type][0],
+				karaokesI18n && karaokesI18n[karaoke[karaoke.from_display_type][0].tid],
+			),
 			slug: slug(karaoke[karaoke.from_display_type][0].name),
 			type: karaoke.from_display_type,
-			tag: karaoke[karaoke.from_display_type][0]
+			tag: karaoke[karaoke.from_display_type][0],
 		};
 	}
 	if (karaoke.series[0]) {
@@ -236,23 +299,24 @@ export function getSerieOrSingerGroupsOrSingers(karaoke: DBKara, karaokesI18n?: 
 			name: getTagInLocale(karaoke.series[0], karaokesI18n && karaokesI18n[karaoke.series[0].tid]),
 			slug: slug(karaoke.series[0].name),
 			type: 'series',
-			tag: karaoke.series[0]
+			tag: karaoke.series[0],
 		};
 	} else if (karaoke.singergroups[0]) {
 		return {
 			name: getTagInLocale(karaoke.singergroups[0], karaokesI18n && karaokesI18n[karaoke.singergroups[0].tid]),
 			slug: slug(karaoke.singergroups[0].name),
 			type: 'singergroups',
-			tag: karaoke.singergroups[0]
+			tag: karaoke.singergroups[0],
 		};
 	} else if (karaoke.singers[0]) {
 		return {
 			name: getTagInLocale(karaoke.singers[0], karaokesI18n && karaokesI18n[karaoke.singers[0].tid]),
 			slug: slug(karaoke.singers[0].name),
 			type: 'singers',
-			tag: karaoke.singers[0]
+			tag: karaoke.singers[0],
 		};
-	} else { // You never know~
+	} else {
+		// You never know~
 		throw new TypeError('The karaoke does not have any series nor singers, wtf?');
 	}
 }
