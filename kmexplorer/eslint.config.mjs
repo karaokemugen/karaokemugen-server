09dd48a5b3efefedc5b693cae566431f85bb7527
Florent Berthelot <florentius.b@gmail.com>
2025-10-04 15:31:36 +0200

    🔧 Ignore unused vars with _ in Nuxt

--- kmexplorer/eslint.config.mjs
+++ kmexplorer/eslint.config.mjs
@@ -1,4 +1,19 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt();
+export default withNuxt({
+	rules: {
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{
+				args: 'all',
+				argsIgnorePattern: '^_',
+				caughtErrors: 'all',
+				caughtErrorsIgnorePattern: '^_',
+				destructuredArrayIgnorePattern: '^_',
+				varsIgnorePattern: '^_',
+				ignoreRestSiblings: true,
+			},
+		],
+	},
+});
