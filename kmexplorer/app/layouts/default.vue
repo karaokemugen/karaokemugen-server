3df2c8f386660e1b202699d3f997a087c4618b39
Florent Berthelot <florentius.b@gmail.com>
2025-12-22 13:20:14 +0100

    🐛 Fix routing to playlist

--- kmexplorer/app/layouts/default.vue
+++ kmexplorer/app/layouts/default.vue
@@ -1240,7 +1240,7 @@
 	});
 
 	const tagType = computed(() => params?.id && (params?.id as string).substring(36));
-	const onKaraTagUserListView = computed(() => ['types-id', 'search-query', 'user-login', 'users', 'suggest', 'index'].includes(name as string));
+	const onKaraTagUserListView = computed(() => ['types-id', 'types-years', 'search-query', 'user-login', 'user-login-animelist', 'users', 'suggest', 'playlists', 'playlist-slug', 'user-login-submissions', 'index'].includes(name as string));
 	const availableLocales = computed(() => locales.value?.filter((i: LocaleObject) => i.code && i.code !== locale.value));
 
 	onMounted(() => {
